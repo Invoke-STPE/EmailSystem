2f7027272e272fd7a2b3e06f708ff630421b6a8d
Steven Pedersen <47424622+Invoke-STPE@users.noreply.github.com>
2022-03-28 10:05:36 +0200

    Add project files.

---
+++ EmailSystem.DL/AppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmailSystem.DL
+{
+    /// <summary>
+    /// Need to configure my AppContext to use default Identity user
+    /// </summary>
+    internal class AppContext : DbContext
+    {
+
+    }
+}
---
+++ EmailSystem.DL/Extensions/AppContextServices.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmailSystem.DL.Extensions
+{
+    public static class AppContextServices
+    {
+        public static IServiceCollection AddAppContext(IServiceCollection services,
+                                                        string connectionString)
+        {
+            services.AddDbContext<AppContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+
+            return services;
+        }
+    }
+}
