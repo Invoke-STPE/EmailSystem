11276c44681c2455363e847a27a0fe9a46867ea8
Steven Pedersen <47424622+Invoke-STPE@users.noreply.github.com>
2022-03-29 09:22:05 +0200

    Update Startup.cs
    Removed unnecessary usings from startup.cs

--- EmailSystem.Web/Startup.cs
+++ EmailSystem.Web/Startup.cs
@@ -1,20 +1,10 @@
+using EmailSystem.DL.Extensions;
 using EmailSystem.Web.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using EmailSystem.DL.Extensions;
 
 namespace EmailSystem.Web
 {
